bf0e922630ee7693f79392f15a9a06b687f4feba
Ahmad Saleem <121984728+ahmad-saleem-23@users.noreply.github.com>
2023-03-30 12:11:30 +1300

    reducer is rendering

--- client/components/GamesList.tsx
+++ client/components/GamesList.tsx
@@ -1,10 +1,22 @@
+import { useState } from 'react'
+
+import {useAppSelector } from '../hooks/hooks'
+
 export default function GamesList() {
+  const games = useAppSelector((state) => state.gamesReducer)
+
   return (
     <>
       <div>
         <h1>My Game Library</h1>
         <ul>
-          <li>game 1</li>
+          {games.data.map(game => (
+            <li key={game.id}>
+              <h2>{game.title}</h2>
+              <p>Genre: {game.genre}</p>
+              <p>Score: {game.score}</p>
+            </li>
+          ))}
         </ul>
       </div>
     </>
--- client/index.tsx
+++ client/index.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import store from './store'
 
+
 import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {

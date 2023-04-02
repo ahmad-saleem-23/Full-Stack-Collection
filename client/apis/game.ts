030116341648030ed47b5cc294203e868f483aeb
Ahmad Saleem <121984728+ahmad-saleem-23@users.noreply.github.com>
2023-04-03 11:45:06 +1200

    added api requests

--- client/apis/game.ts
+++ client/apis/game.ts
@@ -1,5 +1,5 @@
 import request from 'superagent'
-import type {game, Games} from '../../models/games'
+import type {game, Games, newGame, updatedGame} from '../../models/games'
 
 const url ='/v1/games'
 
@@ -10,3 +10,25 @@ return request.get(url).then((response)=>{
  return response.body
 })
 }
+
+export function getGameID (id:number){
+  return request.get(url+ `${id}`).then((response)=>{
+    console.log(response.body)
+   return response.body
+  })
+  }
+
+
+
+export function addAGame (game:newGame){
+  return request.post(url +'add').send(game)
+  }
+
+
+  export function deleteAGame (id:number){
+    return request.del(url +'delete')
+    }
+
+    export function updateAGame (id:number, game:updatedGame){
+      return request.patch(url +`${id}`).send(game)
+    }
